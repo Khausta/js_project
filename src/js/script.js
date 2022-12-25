3603222bccd0b9e6e1a45a2c2f5407998eb9d444
Khausta <anna.a.khaustova@gmail.com>
2022-12-25 11:56:54 +0300

    js modified. Cycles for, while, do ... whilw are added

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
-let money = prompt('Ваш бюджет на месяц?', '');
-let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let money = +prompt('Ваш бюджет на месяц?', '');  //унарный плюс перед prompt для 
+let time  = prompt('Введите дату в формате YYYY-MM-DD', '');
 // let firstQuestion = prompt('Введите обязательную статью расходов в этом месяце', '');
 // let secondQuestion = prompt('Во сколько обойдется?', ''); 
 
@@ -11,22 +11,73 @@ let time = prompt('Введите дату в формате YYYY-MM-DD', '');
 // console.log(expenses.firstQuestion, ':' , expenses.secondQuestion);
 
 let appData = {
-    butget: money,
+    budjet: money,
     time: time,
     expenses: {},
     optionalExpenses: '',
     income: '',
     saving: false
 };
-let a1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-    a2 = prompt('Во сколько обойдется?', ''),
-    a3 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-    a4 = prompt('Во сколько обойдется?', '');
+// let a1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//     a2 = prompt('Во сколько обойдется?', ''),
+//     a3 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//     a4 = prompt('Во сколько обойдется?', '');                               перенесено в цикл for ниже
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
-console.log(appData.expenses);
+// appData.expenses.a1 = a2;
+// appData.expenses.a3 = a4;  изменено в цикле for
+
+// for (let i = 0; i < 2; i++) {
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//         b = prompt('Во сколько обойдется?', '');
+        
+//         if ((typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+//             console.log('done');
+//             appData.expenses[a] = b;
+//         } else {
+
+//         }
+// };
+
+// appData.moneyPerDay = appData.budjet / 30;
 
 
+// alert('Ежедневный бюджет: ' + appData.moneyPerDay);
 
-alert(appData.butget / 30);
+// if(appData.moneyPerDay < 100) {
+//     console.log('Минимальный уровень достатка')
+
+// } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+//     console.log('Средний уровень достатка')
+// } else if (appData.moneyPerDay > 2000) {
+//     console.log('Высокий уровень достатка');
+// } else {
+//     console.log('Произошла ошибка')
+// }
+
+let i = 0;
+
+// while (i < 2) {
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//         b = prompt('Во сколько обойдется?', '');
+        
+//         if ((typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+//             console.log('done');
+//             appData.expenses[a] = b;
+//         } else {
+
+//         }
+//     i++;
+// }
+
+do {
+    let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        b = prompt('Во сколько обойдется?', '');
+        
+        if ((typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+            console.log('done');
+            appData.expenses[a] = b;
+        } else {
+
+        }
+    i++;
+} while (i < 2);
